8071d9646475257d8e5d0a742877b6ba172475f7
Ilya Pakhomov <ipakhomov@ronasit.com>
2025-04-29 19:08:11 +0600

    feat: ability to customize token expiry callback for interceptor

--- src/types/refresh-token-options.ts
+++ src/types/refresh-token-options.ts
@@ -4,6 +4,7 @@ import { AuthConfiguration } from './auth-configuration';
 export type RefreshTokenInterceptorOptions = {
   configuration: AuthConfiguration;
   getIsAuthenticated: () => boolean | null;
+  getIsTokenExpired?: () => boolean | null;
   runTokenRefreshRequest: () => Promise<string>;
   onError: (error: AxiosError<{ error?: string }>) => Promise<void>;
 };
